9ec76b290c899f1d427e37ab32fc1256f3e23cf1
Nayla Hanegan <NaylaHanegan@gmail.com>
2025-06-13 19:53:49 -0400

    add logo stuff

--- fursvp/urls.py
+++ fursvp/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tinymce/', include('tinymce.urls')),
     path('', include('events.urls')),
     path('accounts/', include('users.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
