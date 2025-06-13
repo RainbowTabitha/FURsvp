1615848b0a1f50d7b1e1554940a4ad7863fcb55a
Tabitha Hanegan <tabithahanegan@gmail.com>
2025-06-13 16:17:55 -0400

    FIX CORS

--- fursvp/settings.py
+++ fursvp/settings.py
@@ -41,9 +41,11 @@ INSTALLED_APPS = [
     'users',
     'widget_tweaks',
     'tinymce',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,6 +53,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    r"^https://([a-zA-Z0-9-]+\.)*fursvp\.org$"
 ]
 
 ROOT_URLCONF = 'fursvp.urls'
