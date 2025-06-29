45ae42136220f8304116f6832f567453bc3489b7
Tabitha Hanegan <tabithahanegan@gmail.com>
2025-06-28 20:16:28 -0400

    Revert "fix tg"
    This reverts commit 9f9e816d9fb25186fe1ab51df8e426e142280eb1.

--- users/context_processors.py
+++ users/context_processors.py
@@ -1,5 +1,4 @@
 from django.core.cache import cache
-from django.conf import settings
 
 def banner_settings(request):
     """Context processor to make banner settings available in all templates"""
@@ -14,16 +13,10 @@ def banner_settings(request):
         if banner_type not in valid_types:
             banner_type = 'info'
         
-        # Add Telegram settings
-        telegram_login_enabled = getattr(settings, 'TELEGRAM_LOGIN_ENABLED', False)
-        telegram_bot_username = getattr(settings, 'TELEGRAM_BOT_USERNAME', '')
-        
         return {
             'banner_enabled': bool(banner_enabled),
             'banner_text': str(banner_text),
             'banner_type': str(banner_type),
-            'telegram_login_enabled': telegram_login_enabled,
-            'telegram_bot_username': telegram_bot_username,
         }
     except Exception:
         # Return default values if there's any error
@@ -31,6 +24,4 @@ def banner_settings(request):
             'banner_enabled': False,
             'banner_text': '',
             'banner_type': 'info',
-            'telegram_login_enabled': False,
-            'telegram_bot_username': '',
         } 
