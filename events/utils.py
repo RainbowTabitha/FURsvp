36caacbd899016d51b7e17aab0ac4c9013dcd232
Tabitha Hanegan <tabithahanegan@gmail.com>
2025-07-22 16:36:20 -0400

    der

--- events/utils.py
+++ events/utils.py
@@ -15,7 +15,7 @@ def post_to_telegram_channel(channel, message):
     bot_token = os.environ.get('TELEGRAM_BOT_TOKEN', 'YOUR_TELEGRAM_BOT_TOKEN')
     chat_id = f"@{channel.strip()}"
     url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
-    payload = {"chat_id": chat_id, "text": message}
+    payload = {"chat_id": f'{#}chat_id, "text": message}
     try:
         response = requests.post(url, json=payload, timeout=5)
         response.raise_for_status()
